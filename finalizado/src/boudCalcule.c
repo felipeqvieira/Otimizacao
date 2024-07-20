1525dda76869c9f44e200043737c1f754f28c216
Leonardo Martin <leowork2002@gmail.com>
2024-07-20 18:02:40 -0300

    other bounding

--- finalizado/src/boudCalcule.c
+++ finalizado/src/boudCalcule.c
@@ -43,3 +43,25 @@ int boundCalcule(conjunto_t *cl, Result *result, Remaining *remaining){
 
 
 }
+
+int boundCalcule2(conjunto_t *cl, Result *result, Remaining *remaining)
+{
+  /* 
+    B(E,F) = Número de candidatos escolhidos + 
+    (Nº grupos não cobertos / número máximo de grupos que um candidato restante cobre)
+  */
+
+  int num_remaining_groups = remaining->remainingGroups->card;
+
+  /* Número máximo de grupos que um candidato restante cobre */
+  int max_groups_per_candidate = 0;
+
+  for (int i = 0; i < cl->card; i++)
+  {
+    if(candidates[cl->v[i]].numGroups > max_groups_per_candidate)
+      max_groups_per_candidate = candidates[cl->v[i]].numGroups;
+  }
+  
+  /* B(E,F) =  |E| + MinCandidates */
+  return result->solution->card + ceil(num_remaining_groups / max_groups_per_candidate);
+}
